80fee2e1e58c8718d0d809eb7a5f8ac04c63a579
Tommi42 <t.cambursano@gmail.com>
2024-11-17 11:43:26 +0100

    basic-working-app

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
-from utils import add_message, ai_resposne
+from utils import add_message, ai_resposne, read_random_info, random_GNR
 
 load_dotenv(".env.dev")
+
+def reload_page():
+    st.rerun()
 
 ### Initial setup when loading chat ###
 # Initialize chat history
@@ -21,12 +24,35 @@ st.title("Natalino")
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+if "random_info" not in st.session_state:
+    st.session_state.random_info = read_random_info()["list"]
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    if message["role"] != "system":
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+if len(st.session_state.messages) == 1:
+    left, right = st.columns(2)
+    n = 4 # Number of random info
+    r = random_GNR(n)
+    for i in range(n//2):
+        i = next(r)
+        if left.button(st.session_state.random_info[i], use_container_width=True, key=f'left_button{i}'):
+            add_message("user", st.session_state.random_info[i])
+            reload_page()
+        i = next(r)
+        if right.button(st.session_state.random_info[i], use_container_width=True, key=f'left_button{i}'):
+            add_message("user", st.session_state.random_info[i])
+            reload_page()
+else:
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        if message["role"] != "system":
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+    if st.session_state.messages[-1]["role"] == "user":
+        with st.chat_message("assistant"):
+            stream_response = ai_resposne(st.session_state.messages, st.session_state.openai_client)
+            response = st.write_stream(stream_response)
+            # Add assistant response to chat history
+        add_message("assistant", response)
 
 # Accept user input
 if user_input := st.chat_input("What is up?"):
@@ -40,6 +66,5 @@ if user_input := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         stream_response = ai_resposne(st.session_state.messages, st.session_state.openai_client)
         response = st.write_stream(stream_response)
-
-    # Add assistant response to chat history
+        # Add assistant response to chat history
     add_message("assistant", response)
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import json
+import random
 
 def add_message(role, content):
     st.session_state.messages.append({"role": role, "content": content})
@@ -11,3 +13,13 @@ def ai_resposne(messages, openai_client):
          stream=True
      )
      return stream_response
+
+def read_random_info():
+    with open("random_info.json", "r") as file:
+        return json.load(file)
+
+def random_GNR(n):
+    l = list(range(0, n, 1))
+    random.shuffle(l)
+    for i in l:
+        yield i
