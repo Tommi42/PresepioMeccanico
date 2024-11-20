8d99cacb4e48ed092b374ed6492996567871e850
Tommi42 <t.cambursano@gmail.com>
2024-11-20 16:43:07 +0100

    assitant-version-first-commit

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
-from utils import add_message, ai_resposne, read_random_info, random_GNR
+from utils import add_message, ai_resposne, create_thread, read_random_info, random_GNR
 
 load_dotenv(".env.dev")
 
@@ -12,11 +12,12 @@ def reload_page():
 ### Initial setup when loading chat ###
 # Initialize chat history
 if "messages" not in st.session_state:
-    initial_prompt = open("context_information.txt", "r").read()
-    st.session_state.messages = [{"role": "system", "content": initial_prompt}]
+    st.session_state.messages = []
 if "openai_client" not in st.session_state:
     st.session_state.openai_client = OpenAI(
     )
+if "thread" not in st.session_state:
+    st.session_state.thread = create_thread()
 
 # Page
 st.title("Natalino")
@@ -27,18 +28,18 @@ if "messages" not in st.session_state:
 if "random_info" not in st.session_state:
     st.session_state.random_info = read_random_info()["list"]
 
-if len(st.session_state.messages) == 1:
+if len(st.session_state.messages) == 0:
     left, right = st.columns(2)
     n = 4 # Number of random info
     r = random_GNR(n)
     for i in range(n//2):
         i = next(r)
         if left.button(st.session_state.random_info[i], use_container_width=True, key=f'left_button{i}'):
-            add_message("user", st.session_state.random_info[i])
+            add_message(st.session_state.messages ,"user", st.session_state.random_info[i])
             reload_page()
         i = next(r)
         if right.button(st.session_state.random_info[i], use_container_width=True, key=f'left_button{i}'):
-            add_message("user", st.session_state.random_info[i])
+            add_message(st.session_state.messages, "user", st.session_state.random_info[i])
             reload_page()
 else:
     # Display chat messages from history on app rerun
@@ -52,12 +53,11 @@ else:
             stream_response = ai_resposne(st.session_state.messages, st.session_state.openai_client)
             response = st.write_stream(stream_response)
             # Add assistant response to chat history
-        add_message("assistant", response)
 
 # Accept user input
-if user_input := st.chat_input("What is up?"):
+if user_input := st.chat_input('Chiedi a natalino'):
     # Add user message to chat history
-    add_message("user", user_input)
+    add_message(st.session_state.messages, "user", user_input)
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(user_input)
@@ -67,4 +67,3 @@ if user_input := st.chat_input("What is up?"):
         stream_response = ai_resposne(st.session_state.messages, st.session_state.openai_client)
         response = st.write_stream(stream_response)
         # Add assistant response to chat history
-    add_message("assistant", response)
--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,43 @@
+from numpy import prod
 import streamlit as st
 import json
 import random
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv(".env.dev")
+client = OpenAI()
 
-def add_message(role, content):
-    st.session_state.messages.append({"role": role, "content": content})
+def message_streamer(messages,run):
+    response = ''
+    try:
+        for event in run:
 
+            try:
+                if event.event == "thread.message.delta":
+                    response += str(event.data.delta.content[0].text.value)
+                    yield str(event.data.delta.content[0].text.value)
+            except:
+                pass
+
+    finally:
+        messages.append({'role': 'assistant', 'content': response})
+
+def add_message(messages, role, content):
+    thread_message = client.beta.threads.messages.create(
+        thread_id=st.session_state.thread.id,
+        role=role,
+        content=content
+    )
+    messages.append({'role': role, 'content': content})
 
 def ai_resposne(messages, openai_client):
-     stream_response = openai_client.chat.completions.create(
-         model="gpt-4o-mini",
-         messages=messages,
-         stream=True
+     run = client.beta.threads.runs.create(
+       thread_id=st.session_state.thread.id,
+       assistant_id=str(os.environ["ASSISTANT_ID"]),
+       stream=True
      )
-     return stream_response
+     return message_streamer(messages, run)
 
 def read_random_info():
     with open("random_info.json", "r") as file:
@@ -23,3 +48,7 @@ def random_GNR(n):
     random.shuffle(l)
     for i in l:
         yield i
+
+def create_thread():
+    thread = client.beta.threads.create()
+    return thread
