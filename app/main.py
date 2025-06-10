98a682653867f9cfa8d0ce3fd1118608daaf15e0
Tommi42 <t.cambursano@gmail.com>
2025-06-10 15:44:27 +0100

    text suggestions

--- app/main.py
+++ app/main.py
@@ -5,15 +5,13 @@ import time
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
+import random
 
 from utils import add_message, ai_resposne, read_random_info, random_GNR
 from pip._vendor.requests import delete
 
 st.set_page_config(page_title="Natalino", layout="wide")
 load_dotenv(".env.dev")
-
-print("Nuovo Timer")
-timer = time.time()
 
 def reload_page():
     st.rerun()
@@ -77,7 +75,9 @@ def delete_header():
         </style>
     """, unsafe_allow_html=True)
 
-add_custom_css()
+
+
+#add_custom_css()
 set_chat_input_width(900)
 set_background('./static/background.jpg')
 delete_header()
@@ -93,7 +93,23 @@ if "openai_client" not in st.session_state:
     )
 if "random_info" not in st.session_state:
     st.session_state.random_info = read_random_info()["list"]
+    random.shuffle(st.session_state.random_info)
 
+
+def menage_message(input):
+    # Add user message to chat history
+    user_input = input
+    add_message("user", user_input)
+    reload_page()
+
+    # Display assistant response in chat message container
+    with messages.chat_message("assistant"):
+        stream_response = ai_resposne(st.session_state.messages, st.session_state.openai_client)
+        response = st.write_stream(stream_response)
+        # Add assistant response to chat history
+    add_message("assistant", response)
+
+n_random = len(st.session_state.random_info)
 
 c1, c2 = st.columns([1,3])
 
@@ -103,11 +119,12 @@ with c1:
         st.session_state.messages = [{"role": "system", "content": initial_prompt}]
         reload_page()
     st.title("Natalino")
-    #st.image("./static/Natalino.png", width=130)
+    st.image("./static/Natalino.png", width=130)
 
 with c2:
-    messages = st.container(border=True, height=600)
+    messages = st.container(border=False, height=600)
     if len(st.session_state.messages) != 1:
+        print(len(st.session_state.messages))
         # Display chat messages from history on app rerun
         for message in st.session_state.messages:
             if message["role"] != "system":
@@ -121,21 +138,18 @@ with c2:
                 # Add assistant response to chat history
             add_message("assistant", response)
     else:
-        messages.markdown("<p style='color: #D9D9D9; font-size: 30px;'>Ciao, sono Natalino!</p>", unsafe_allow_html=True)
-        messages.markdown("<p style='color: #D9D9D9; font-size: 30px;'>Chiedimi ciò che vuoi sul Presepio Meccanico!</p>", unsafe_allow_html=True)
+        messages.markdown("<p style='color: #FFFFFF; font-size: 30px;'>Chiedimi ciò che vuoi sul Presepio Meccanico!</p>", unsafe_allow_html=True)
 
+        grid_c1, grid_c2 = messages.columns([1,1])
+
+        if grid_c1.button(st.session_state.random_info[0], use_container_width=True):
+            menage_message(st.session_state.random_info[0])
+        elif grid_c1.button(st.session_state.random_info[1], use_container_width=True):
+            menage_message(st.session_state.random_info[1])
+        elif grid_c2.button(st.session_state.random_info[2], use_container_width=True):
+            menage_message(st.session_state.random_info[2])
+        elif grid_c2.button(st.session_state.random_info[3], use_container_width=True):
+            menage_message(st.session_state.random_info[3])
 
     if user_input := st.chat_input("Scrivi qui"):
-        # Add user message to chat history
-        add_message("user", user_input)
-        # Display user message in chat message container
-        with messages.chat_message("user"):
-            st.markdown(user_input)
-
-        # Display assistant response in chat message container
-        with messages.chat_message("assistant"):
-            stream_response = ai_resposne(st.session_state.messages, st.session_state.openai_client)
-            response = st.write_stream(stream_response)
-            # Add assistant response to chat history
-        add_message("assistant", response)
-        reload_page()
+        menage_message(user_input)
