ddca7f1e798fa27614e3cc8904a0d940e3d63383
Tommi <t.cambursano@gmail.com>
2025-11-05 21:56:17 +0100

    web_version

--- app/main.py
+++ app/main.py
@@ -15,16 +15,22 @@ load_dotenv(".env.dev")
 print("Nuovo Timer")
 timer = time.time()
 
+
 def reload_page():
     st.rerun()
+
+
 def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
+    with open(bin_file, "rb") as f:
         data = f.read()
         print("File read as binary")
     return base64.b64encode(data).decode()
+
+
 def set_background(png_file):
     bin_str = get_base64(png_file)
-    page_bg_img = '''
+    page_bg_img = (
+        """
     <style>
     .stApp {
     background-image: url("data:image/png;base64,%s");
@@ -32,8 +38,12 @@ def set_background(png_file):
     }
 
     </style>
-    ''' % bin_str
+    """
+        % bin_str
+    )
     st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 def add_custom_css():
     st.markdown(
         """
@@ -49,8 +59,10 @@ def add_custom_css():
 
         </style>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
+
+
 def set_chat_input_width(width):
     st.markdown(
         """
@@ -60,14 +72,19 @@ def set_chat_input_width(width):
             justify-content: center;
         }
         div[data-testid="stChatInput"] > div {
-            width: """ + str(width) + """px;
+            width: """
+        + str(width)
+        + """px;
         }
         </style>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
+
+
 def delete_header():
-    st.markdown("""
+    st.markdown(
+        """
         <style>
             .reportview-container {
                 margin-top: -2em;
@@ -75,10 +92,14 @@ def delete_header():
             .stAppHeader {display: none;}
             .stMainBlockContainer{padding-top: 10px;}
         </style>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
+
+
 def set_chat_input_full_width():
-        st.markdown(
-            """
+    st.markdown(
+        """
             <style>
             div[data-testid="stChatInput"] {
                 width: 100vw !important;
@@ -89,8 +110,9 @@ def set_chat_input_full_width():
             }
             </style>
             """,
-            unsafe_allow_html=True
-        )
+        unsafe_allow_html=True,
+    )
+
 
 def set_chat_input_c2_width():
     st.markdown(
@@ -104,8 +126,10 @@ def set_chat_input_c2_width():
         }
         </style>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
+
+
 def set_title_yellow():
     st.markdown(
         """
@@ -115,13 +139,12 @@ def set_title_yellow():
         }
         </style>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
 
 
-
 add_custom_css()
-set_background('./static/Sfondo05.png')
+set_background("./app/static/Sfondo05.png")
 set_chat_input_c2_width()
 delete_header()
 set_title_yellow()
@@ -133,24 +156,23 @@ if "messages" not in st.session_state:
     initial_prompt = open("context_information.txt", "r").read()
     st.session_state.messages = [{"role": "system", "content": initial_prompt}]
 if "openai_client" not in st.session_state:
-    st.session_state.openai_client = OpenAI(
-    )
+    st.session_state.openai_client = OpenAI()
 if "random_info" not in st.session_state:
     st.session_state.random_info = read_random_info()["list"]
 
 
-c1, c2 = st.columns([3,2])
+c1, c2 = st.columns([3, 2])
 
 user_input = c2.chat_input(placeholder="Scrivi qui")
 
 
 with c1:
-    v1, v2 = st.columns([1,2])
+    v1, v2 = st.columns([1, 2])
 
     with v1:
         st.container(width="stretch", height=100, border=False)
         st.title("Natalino")
-        st.image("./static/Natalino02.png", width=250)
+        st.image("./app/static/Natalino02.png", width=250)
 
     with v2:
         st.container(width="stretch", height=100, border=False)
@@ -159,7 +181,9 @@ with c1:
             add_message("user", user_input)
             # Display assistant response in chat message container
             with st.chat_message("natalino", avatar="🎅"):
-                stream_response = ai_resposne(st.session_state.messages, st.session_state.openai_client)
+                stream_response = ai_resposne(
+                    st.session_state.messages, st.session_state.openai_client
+                )
                 response = st.write_stream(stream_response)
                 # Add assistant response to chat history
             add_message("assistant", response)
@@ -169,7 +193,5 @@ with c2:
     st.subheader("Sono qui per aiutarti a scoprire tutto su Presepio Meccanico!")
     st.image("./static/Falegnami03.png", width=500)
 
-
-
-    #st.markdown("<p style='color: #D9D9D9; font-size: 30px;'>Ciao, sono Natalino!</p>", unsafe_allow_html=True)
-    #st.markdown("<p style='color: #D9D9D9; font-size: 30px;'>Chiedimi ciò che vuoi sul Presepio Meccanico!</p>", unsafe_allow_html=True)
+    # st.markdown("<p style='color: #D9D9D9; font-size: 30px;'>Ciao, sono Natalino!</p>", unsafe_allow_html=True)
+    # st.markdown("<p style='color: #D9D9D9; font-size: 30px;'>Chiedimi ciò che vuoi sul Presepio Meccanico!</p>", unsafe_allow_html=True)
