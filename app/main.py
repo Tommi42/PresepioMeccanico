f6afc8db202cdc832f1fab378529e8524b05e327
Tommi <t.cambursano@gmail.com>
2025-11-08 15:47:33 +0100

    frontend-init

--- app/main.py
+++ app/main.py
@@ -156,6 +156,17 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+st.markdown(
+    """
+    <style>
+    h2 {
+        color: #fff !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 
 add_custom_css()
 set_background("./app/static/Sfondo05.png")
@@ -208,7 +219,7 @@ with c1:
             # Add user message to chat history
             add_message("user", user_input)
             # Display assistant response in chat message container
-            with st.chat_message("natalino", avatar="🎅"):
+            with st.chat_message("natalino", avatar=":)"):
                 stream_response = ai_resposne(
                     st.session_state.messages, st.session_state.openai_client
                 )
